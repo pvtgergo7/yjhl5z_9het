55e96428345ade107b4d645ad4102066d77f76ed
Gergő Vágner <gergo.vagner@stud.uni-corvinus.hu>
2022-12-07 16:23:42 +0100

    a

--- excelgyak/Form1.cs
+++ excelgyak/Form1.cs
@@ -1,10 +1,107 @@
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
 namespace excelgyak
 {
     public partial class Form1 : Form
     {
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB;     // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+
         public Form1()
         {
             InitializeComponent();
+        }
+        public void CreateExcel()
+        {
+            try
+            {
+               
+                xlApp = new Excel.Application();
+
+            
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+               
+                xlSheet = xlWB.ActiveSheet;
+
+               
+                CreateTable(); 
+
+                
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) 
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+        void CreateTable()
+        {
+            string[] fejlécek = new string[] {
+        "Kérdés",
+        "1. válasz",
+        "2. válaszl",
+        "3. válasz",
+        "Helyes válasz",
+        "kép"};
+            for (int i = 0; i < fejlécek.Length; i++)
+            {
+                xlSheet.Cells[1, 1] = fejlécek[0];
+            }
+            Models.HajosContext hajosContext = new Models.HajosContext();
+            var mindenKérdés = hajosContext.Questions.ToList();
+            object[,] adatTömb = new object[mindenKérdés.Count(), fejlécek.Count()];
+            for (int i = 0; i < mindenKérdés.Count(); i++)
+            {
+                adatTömb[i, 0] = mindenKérdés[i].Question1;
+                adatTömb[i, 1] = mindenKérdés[i].Answer1;
+                adatTömb[i, 2] = mindenKérdés[i].Answer2;
+                adatTömb[i, 3] = mindenKérdés[i].Answer3;
+                adatTömb[i, 4] = mindenKérdés[i].CorrectAnswer;
+                adatTömb[i, 5] = mindenKérdés[i].Image;
+            }
+            int sorokSzáma = adatTömb.GetLength(0);
+            int oszlopokSzáma = adatTömb.GetLength(1);
+            Excel.Range adatRange = xlSheet.get_Range("A2", Type.Missing).get_Resize(sorokSzáma, oszlopokSzáma);
+            adatRange.Value2 = adatTömb;
+            adatRange.Columns.AutoFit();
+            Excel.Range fejllécRange = xlSheet.get_Range("A1", Type.Missing).get_Resize(1, 6);
+            fejllécRange.Font.Bold = true;
+            fejllécRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            fejllécRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            fejllécRange.EntireColumn.AutoFit();
+            fejllécRange.RowHeight = 40;
+            fejllécRange.Interior.Color = Color.Fuchsia;
+            fejllécRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+            Excel.Range fejlec = xlSheet.get_Range("A1", Type.Missing).get_Resize(1, 6);
+            fejlec.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
         }
     }
 }
